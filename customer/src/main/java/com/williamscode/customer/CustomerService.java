556aa568ef3a22a82403d8788470e424ea8b97c2
williamscode <uz.williamscode@gmail.com>
2024-01-21 14:21:59 +0500

    service discovery with Eureka

--- customer/src/main/java/com/williamscode/customer/CustomerService.java
+++ customer/src/main/java/com/williamscode/customer/CustomerService.java
@@ -17,7 +17,7 @@ public record CustomerService(CustomerRepository customerRepository, RestTemplat
         //todo check if lastName valid
         //todo check if email valid
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
